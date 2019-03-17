e5a7476b7827c100c56fbe576b4d3d17be5de95f
cswksu@gmail.com <cswksu@gmail.com>
2019-03-17 19:28:02 -0400

    feat: add member variables
    Add member variables pose, base_waypoints, waypoints_2d, and waypoint_tree to WaypointUpdater on init.

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -36,6 +36,10 @@ class WaypointUpdater(object):
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
         # TODO: Add other member variables you need below
+        self.pose = None
+        self.base_waypoints = None
+        self.waypoints_2d = None
+        self.waypoint_tree = None
 
         rospy.spin()
 
