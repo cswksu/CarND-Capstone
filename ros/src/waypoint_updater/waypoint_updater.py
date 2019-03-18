62e911977bcf2008066e6999a03393cb3ae0af8e
cswksu@gmail.com <cswksu@gmail.com>
2019-03-17 22:24:02 -0400

    fix: typo

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -47,7 +47,7 @@ class WaypointUpdater(object):
 
     def loop(self):
         rate = rospy.Rate(50)
-        while not rospy.is_shotdown():
+        while not rospy.is_shutdown():
             if self.pose and self.base_waypoints:
                 #Get closest waypoint
                 closest_waypoint_idx = self.get_closest_waypoint_idx()
