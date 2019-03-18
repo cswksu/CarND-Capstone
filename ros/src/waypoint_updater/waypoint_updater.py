316ba881b07d502e3468e89837443fb12dc88a5c
cswksu@gmail.com <cswksu@gmail.com>
2019-03-17 22:39:54 -0400

    fix: remove Int32 import from styx_msgs

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -3,7 +3,7 @@
 import numpy as np
 import rospy
 from geometry_msgs.msg import PoseStamped
-from styx_msgs.msg import Lane, Waypoint, Int32
+from styx_msgs.msg import Lane, Waypoint
 from scipy.spatial import KDTree
 
 import math
