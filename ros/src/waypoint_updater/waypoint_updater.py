26566a0e7ee581a51edf05503ebb9ae5fc160fea
cswksu@gmail.com <cswksu@gmail.com>
2019-03-18 01:07:45 -0400

    fix: if closest_waypoint_idx not defined, don't publish

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -52,7 +52,8 @@ class WaypointUpdater(object):
             if self.pose and self.base_waypoints:
                 #Get closest waypoint
                 closest_waypoint_idx = self.get_closest_waypoint_idx()
-                self.publish_waypoints(closest_waypoint_idx)
+                if closest_waypoint_idx:
+                    self.publish_waypoints(closest_waypoint_idx)
             rate.sleep()
 
     def get_closest_waypoint_idx(self):
