8b4bca2175c663dd1e92b0d63334e7b8e47ed95f
Christian Wiles <cswksu@gmail.com>
2019-03-20 22:10:50 -0400

    fix: make vehParams properties of the Controller

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -5,16 +5,18 @@ ONE_MPH = 0.44704
 
 class Controller(object):
     def __init__(self, vehParams):
-        vehicle_mass = vehParams[0]
-        fuel_capacity = vehParams[1]
-        brake_deadband = vehParams[2]
-        decel_limit = vehParams[3]
-        accel_limit = vehParams[4]
-        wheel_radius = vehParams[5]
-        wheel_base = vehParams[6]
-        steer_ratio = vehParams[7]
-        max_lat_accel = vehParams[8]
-        max_steer_angle = vehParams[9]
+        self.vehicle_mass = vehParams[0]
+        self.fuel_capacity = vehParams[1]
+        self.brake_deadband = vehParams[2]
+        self.decel_limit = vehParams[3]
+        self.accel_limit = vehParams[4]
+        self.wheel_radius = vehParams[5]
+        self.wheel_base = vehParams[6]
+        self.steer_ratio = vehParams[7]
+        self.max_lat_accel = vehParams[8]
+        self.max_steer_angle = vehParams[9]
+        
+        
         
         #calculate vehicle mass with fuel
         vehicle_net_mass = vehicle_mass+fuel_capacity * GAS_DENSITY
@@ -22,4 +24,5 @@ class Controller(object):
     def control(self, current_velocity, proposed_linear_velocity, proposed_angular_velocity, drive_by_wire_enabled):
         # TODO: Change the arg, kwarg list to suit your needs
         # Return throttle, brake, steer
+        
         return 1., 0., 0.
