291f454efe415bb39a52dfb35a2f9b9dab68edbd
Christian Wiles <cswksu@gmail.com>
2019-03-20 21:43:44 -0400

    feat: import vehicle parameters from constructor
    Import vehicle parameters from parameter list. Also calculate net weight using fuel density.

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -4,9 +4,20 @@ ONE_MPH = 0.44704
 
 
 class Controller(object):
-    def __init__(self, *args, **kwargs):
-        # TODO: Implement
-        pass
+    def __init__(self, vehParams):
+        vehicle_mass = vehParams[0]
+        fuel_capacity = vehParams[1]
+        brake_deadband = vehParams[2]
+        decel_limit = vehParams[3]
+        accel_limit = vehParams[4]
+        wheel_radius = vehParams[5]
+        wheel_base = vehParams[6]
+        steer_ratio = vehParams[7]
+        max_lat_accel = vehParams[8]
+        max_steer_angle = vehParams[9]
+        
+        #calculate vehicle mass with fuel
+        vehicle_net_mass = vehicle_mass+fuel_capacity * GAS_DENSITY
 
     def control(self, *args, **kwargs):
         # TODO: Change the arg, kwarg list to suit your needs
