5c29e427e077d8bcdadf651fdb975c201d6e9e6e
Christian Wiles <cswksu@gmail.com>
2019-04-23 20:37:03 -0400

    fix: update publishing criteria

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -51,7 +51,7 @@ class WaypointUpdater(object):
     def loop(self):
         rate = rospy.Rate(50)
         while not rospy.is_shutdown():
-            if self.pose and self.base_waypoints:
+            if self.pose and self.base_lane:
                 #Get closest waypoint
                 closest_waypoint_idx = self.get_closest_waypoint_idx()
                 if closest_waypoint_idx:
